d3bb81b383acf48427954acc00f845cc4244d7d0
Ash Garcia <github@garcia.sh>
2019-11-27 20:03:47 -0700

    Updates

--- precache-manifest.23abf6b77889d92e8cfdfc74a69cbacb.js
+++ precache-manifest.23abf6b77889d92e8cfdfc74a69cbacb.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2ebad070d5acaaf270f81f5a28f53629",
+    "revision": "926f3a7d183fc8f53b9b9137748fc8f8",
     "url": "/index.html"
   },
   {
-    "revision": "8c37e035bf05050d7fbe",
-    "url": "/static/css/main.1d0f6bfc.chunk.css"
+    "revision": "1c4b97e2adcc4e1e8989",
+    "url": "/static/css/main.7188dbe9.chunk.css"
   },
   {
     "revision": "e0b19a5bd4c0fb1a621c",
     "url": "/static/js/2.44f61565.chunk.js"
   },
   {
-    "revision": "8c37e035bf05050d7fbe",
-    "url": "/static/js/main.46f798f8.chunk.js"
+    "revision": "1c4b97e2adcc4e1e8989",
+    "url": "/static/js/main.b30bd8d5.chunk.js"
   },
   {
     "revision": "9f5d8a58b0488ee60bb4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.842eabcb9f4a8da8825f2babaa66e743.js"
+  "/precache-manifest.23abf6b77889d92e8cfdfc74a69cbacb.js"
 );
 
 self.addEventListener('message', (event) => {
